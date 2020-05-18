fb0d8f97510347059431188f3c5712d03acb9aae
Derek Xu <xzrderek@gmail.com>
2020-05-17 17:07:02 -0700

    new template test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:meditation_app/main.dart';
+import 'package:flutter_travel_ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
