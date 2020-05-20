202e2fefb9e89d448f548d04454f5db33660216c
Derek Xu <xzrderek@gmail.com>
2020-05-19 20:18:59 -0700

    title fix

--- best_flutter_ui_templates/lib/fitness_app/faq/faq_screen.dart
+++ best_flutter_ui_templates/lib/fitness_app/faq/faq_screen.dart
@@ -207,7 +207,7 @@ class _FaqScreen extends State<FaqScreen>
                               child: Padding(
                                 padding: const EdgeInsets.all(8.0),
                                 child: Text(
-                                  'Settings',
+                                  'FAQs',
                                   textAlign: TextAlign.left,
                                   style: TextStyle(
                                     fontFamily: FintnessAppTheme.fontName,
