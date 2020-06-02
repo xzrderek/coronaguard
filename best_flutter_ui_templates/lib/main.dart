52a2fef76e9f59c4ef5430ac7558c26b370e9e5e
Derek Xu <xzrderek@gmail.com>
2020-06-02 16:05:48 -0700

    clean up 3

--- best_flutter_ui_templates/lib/main.dart
+++ best_flutter_ui_templates/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:best_flutter_ui_templates/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'navigation_home_screen.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         textTheme: AppTheme.textTheme,
         platform: TargetPlatform.iOS,
       ),
-      home: NavigationHomeScreen(),
+      home: FitnessAppHomeScreen(),
     );
   }
 }
